0824a8265e819c3a0958239bc43714b5912d0619
alufers <alufers@wp.pl>
2020-08-15 23:02:14 +0200

    Add log-in via command line arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,24 @@
-#include <stdio.h>
 #include <iostream>
-#include <string.h>
+#include <string>
 #include <PlainConnection.h>
 #include <Session.h>
 #include <authentication.pb.h>
 
+int main(int argc, char **argv)
+{
+    if (argc != 3)
+    {
+        std::cout << "usage:\n";
+        std::cout << "    cspot <username> <password>\n";
+        return 1;
+    }
 
-
-int main()
-{    
     auto connection = new PlainConnection();
     connection->connectToAp();
 
     auto session = new Session();
     session->connect(connection);
-    session->authenticate("log", "pass");
+    session->authenticate(std::string(argv[1]), std::string(argv[2]));
     // test.login_credentials.username
     return 0;
 }
