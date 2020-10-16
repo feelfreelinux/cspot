9d26fbf8ceba6b7575c036f7c4935ea893112fa7
Filip <rj23@protonmail.com>
2020-10-16 13:59:07 +0200

    feat: Fixed connection issue

--- cspot/src/AudioChunkManager.cpp
+++ cspot/src/AudioChunkManager.cpp
@@ -1,9 +1,10 @@
 #include "AudioChunkManager.h"
-#include <algorithm>
 
 AudioChunkManager::AudioChunkManager()
 {
     this->chunks = std::vector<std::shared_ptr<AudioChunk>>();
+    startTask();
+    //this->audioChunkDataQueue = new Queue<std::pair<std::vector<uint8_t>, bool>>;
 }
 
 std::shared_ptr<AudioChunk> AudioChunkManager::registerNewChunk(uint16_t seqId, std::vector<uint8_t> &audioKey, uint32_t startPos, uint32_t endPos)
@@ -14,53 +15,63 @@ std::shared_ptr<AudioChunk> AudioChunkManager::registerNewChunk(uint16_t seqId, 
 
     return chunk;
 }
-
-void AudioChunkManager::handleChunkData(std::vector<uint8_t> data, bool failed)
+void AudioChunkManager::handleChunkData(std::vector<uint8_t>& data, bool failed)
 {
-    uint16_t seqId = ntohs(extract<uint16_t>(data, 0));
+    auto audioPair = std::pair(data, failed);
+    audioChunkDataQueue.push(audioPair);
+}
 
-    // Erase all chunks that are not referenced elsewhere anymore
-    chunks.erase(std::remove_if(
+void AudioChunkManager::runTask() {
+    while(true) {
+        std::pair<std::vector<uint8_t>, bool> audioPair;
+        if (this->audioChunkDataQueue.wpop(audioPair)) {
+            auto data = audioPair.first;
+            auto failed = audioPair.second;
+            uint16_t seqId = ntohs(extract<uint16_t>(data, 0));
+
+            // Erase all chunks that are not referenced elsewhere anymore
+            chunks.erase(std::remove_if(
                      chunks.begin(), chunks.end(),
                      [](const std::shared_ptr<AudioChunk> &chunk) {
                          return chunk.use_count() == 1;
                      }),
                  chunks.end());
 
-    for (auto const &chunk : this->chunks)
-    {
-        // Found the right chunk
-        if (chunk->seqId == seqId)
-        {
-            if (failed)
+            for (auto const &chunk : this->chunks)
             {
-                chunk->isHeaderFileSizeLoadedSemaphore->give();
-                chunk->isLoadedSemaphore->give();
-                return;
-            }
-
-            switch (data.size())
-            {
-            case DATA_SIZE_HEADER:
-            {
-                auto headerSize = ntohs(extract<uint16_t>(data, 2));
-                // Got file size!
-                chunk->headerFileSize = ntohl(extract<uint32_t>(data, 5)) * 4;
-                chunk->isHeaderFileSizeLoadedSemaphore->give();
-                break;
-            }
-            case DATA_SIZE_FOOTER:
-                if (chunk->endPosition > chunk->headerFileSize)
+                // Found the right chunk
+                if (chunk->seqId == seqId)
                 {
-                    chunk->endPosition = chunk->headerFileSize;
-                }
-                //ESP_LOGI("cspot", "ID: %d: Starting decrypt!\n", seqId);
+                    if (failed)
+                    {
+                        chunk->isHeaderFileSizeLoadedSemaphore->give();
+                        chunk->isLoadedSemaphore->give();
+                        break;
+                    }
+
+                    switch (data.size())
+                    {
+                    case DATA_SIZE_HEADER:
+                    {
+                        printf("ID: %d: header decrypt!\n", seqId);
+                        auto headerSize = ntohs(extract<uint16_t>(data, 2));
+                        // Got file size!
+                        chunk->headerFileSize = ntohl(extract<uint32_t>(data, 5)) * 4;
+                        chunk->isHeaderFileSizeLoadedSemaphore->give();
+                        break;
+                    }
+                    case DATA_SIZE_FOOTER:
+                        if (chunk->endPosition > chunk->headerFileSize)
+                        {
+                            chunk->endPosition = chunk->headerFileSize;
+                        }
+                        printf("ID: %d: Starting decrypt!\n", seqId);
                 chunk->decrypt();
                 chunk->isLoadedSemaphore->give();
                 break;
 
             default:
-                // ESP_LOGI("cspot", "ID: %d: Got data chunk!\n", seqId);
+                printf("ID: %d: Got data chunk!\n", seqId);
                 // 2 first bytes are size so we skip it
                 auto actualData = std::vector<uint8_t>(data.begin() + 2, data.end());
                 chunk->appendData(actualData);
@@ -68,4 +79,7 @@ void AudioChunkManager::handleChunkData(std::vector<uint8_t> data, bool failed)
             }
         }
     }
+    }
 }
+}
+
--- targets/esp32/main/main.cpp
+++ targets/esp32/main/main.cpp
@@ -93,6 +93,7 @@ void app_main(void)
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     ESP_ERROR_CHECK(example_connect());
     initialize_sntp();
+        // auto audioSink = std::make_shared<I2SAudioSink>();
 
     // wait for time to be set
     time_t now = 0;
@@ -107,6 +108,6 @@ void app_main(void)
 
     time(&now);
     ESP_LOGI("TAG", "Connected to AP, start spotify receiver");
-
+    // for(;;);
     auto taskHandle = xTaskCreatePinnedToCore(&cspotTask, "cspot", 8192 * 8, NULL, 5, NULL, 0);
 }
