de0cb4ddedecba1d8e378881f7678069982b0aa4
Filip <f.grzywok@gmail.com>
2021-10-26 14:05:25 +0200

    feat: revert linux semaphore

--- cspot/src/platform/linux/WrappedSemaphore.cpp
+++ cspot/src/platform/linux/WrappedSemaphore.cpp
@@ -10,25 +10,9 @@ WrappedSemaphore::~WrappedSemaphore()
     sem_destroy(&this->semaphoreHandle);
 }
 
-int WrappedSemaphore::wait()
+void WrappedSemaphore::wait()
 {
-
-    struct timespec ts;
-    if (clock_gettime(CLOCK_REALTIME, &ts) == -1)
-    {
-        /* handle error */
-        return -1;
-    }
-
-    ts.tv_usec += 1000;
-    while ((s = sem_timedwait(&this->semaphoreHandle, &ts)) == -1 && errno == EINTR)
-        continue;       /* Restart if interrupted by handler */
-    if (s == -1)
-    {
-        return 1;
-    }
-    else
-        return 0;
+    sem_wait(&this->semaphoreHandle);
 }
 
 void WrappedSemaphore::give()
