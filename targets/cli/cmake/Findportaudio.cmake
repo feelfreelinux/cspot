541d1a3b705c5f7a7af72aa34c31adc91dbc3bdb
Filip Grzywok <f.grzywok@gmail.com>
2024-03-24 19:06:56 +0100

    feat: Remove unnecessary portaudio include

--- targets/cli/cmake/Findportaudio.cmake
+++
@@ -1,4 +0,0 @@
-#  PORTAUDIO_FOUND - system has libportaudio
-#  PORTAUDIO_INCLUDE_DIRS - the libportaudio include directory
-#  PORTAUDIO_LIBRARIES - Link these to use libportaudio
-message(STATUS "DON'T USE THIS")
